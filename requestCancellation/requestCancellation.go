5b0e90d00a4200c46c6358d0f1464011b8604aa6
Kunal Taitkar <kunaltaitkar888@gmail.com>
2020-03-13 10:28:31 +0530

    Request cancellation using gin framwork.

---
+++ requestCancellation/requestCancellation.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+
+// problem statement: I want to stop server side processed request when browser is closed.
+// Obeservation : Do not use c as a context from handler func in gin framework (where c is func Test(c *gin.Context))
+//    			  Always use c.Request.Context() function for context
+
+// How to test?
+//Step 1: Start server
+//Step 2: Hit localhost:3030/test in your browser
+//step 3: then close tab or browser
+
+	g := gin.Default()
+	g.GET("/test", Test)
+
+	log.Fatal(g.Run(":3030"))
+
+}
+
+func Test(c *gin.Context) {
+	//do not use c as a context
+	//use c.Request.Context()
+	myFunc(c.Request.Context())
+}
+
+func myFunc(ctx context.Context) {
+	fmt.Println("Process is running!")
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Request Cancelled!")
+			return
+		default:
+			fmt.Println("processing...")
+			// return
+		}
+	}
+
+}
